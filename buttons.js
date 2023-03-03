8fdbfb41639565ea4182848ad6f9a2cf1d3a4aee
Daniel John <daniel.john.projects@gmail.com>
2023-03-03 15:10:09 +1100

    Formatting

--- buttons.js
+++ buttons.js
@@ -1,36 +1,48 @@
 // Clear button functionality
 function clearBoard(board) {
-    board.curves.forEach(
-        (element) => { board.removeObject(element) });
+  board.curves.forEach((element) => {
+    board.removeObject(element);
+  });
 }
-document.getElementById("clear-btn").onclick = function () { clearBoard(zboard); clearBoard(wboard); }
+document.getElementById("clear-btn").onclick = function () {
+  clearBoard(zboard);
+  clearBoard(wboard);
+};
 
 // Go button funcitonality
 /**
  * Sets the active function to the value contained in input element.
  */
 function setActiveFunction() {
-    funcstr = document.getElementById("function-input").value;
-    try {
-        transform_func = math.parse(funcstr);
-        document.querySelector("#activeFunctionReadout").innerHTML = 
-            "Active function: \\(f(z) = " + funcstr + "\\)";
-        MathJax.typeset();
-    }
-    catch(err) {
-        console.log("Invalid function.");
-        placeholder = document.getElementById("alertPlaceholder");
-        var wrapper = document.createElement('div');
-        wrapper.innerHTML = '<div class="alert alert-warning alert-dismissible" role="alert" id="invalidFunctionAlert">Invalid function<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+  funcstr = document.getElementById("function-input").value;
+  try {
+    transform_func = math.parse(funcstr);
+    document.querySelector("#activeFunctionReadout").innerHTML =
+      "Active function: \\(f(z) = " + funcstr + "\\)";
+    MathJax.typeset();
+  } catch (err) {
+    console.log("Invalid function.");
+    placeholder = document.getElementById("alertPlaceholder");
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML =
+      '<div class="alert alert-warning alert-dismissible" role="alert" id="invalidFunctionAlert">Invalid function<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
 
-        placeholder.append(wrapper);
-    }
+    placeholder.append(wrapper);
+  }
 }
-document.getElementById("go-btn").onclick = function () { setActiveFunction(); }
-document.getElementById("function-input").onkeypress = function (e) { if (e.code == "Enter") {setActiveFunction();} }
+document.getElementById("go-btn").onclick = function () {
+  setActiveFunction();
+};
+document.getElementById("function-input").onkeypress = function (e) {
+  if (e.code == "Enter") {
+    setActiveFunction();
+  }
+};
 
 // Colour choice functionality
 var colorOptions = document.getElementsByName("colorOptions");
 for (let option of colorOptions) {
-    option.addEventListener("change", function() { strokeColor = option.value; });
+  option.addEventListener("change", function () {
+    strokeColor = option.value;
+  });
 }
--- geometry.js
+++ geometry.js
@@ -1,16 +1,16 @@
 /**
  * Get the coordinates of an event relative to a board's axes.
- * 
+ *
  * Arguments:
  *  evt - The event object.
  *  board - The JXG.Board on which the event occurred.
- * 
+ *
  * Returns:
- *  The X,Y coordinates of the event in an array. 
+ *  The X,Y coordinates of the event in an array.
  */
- function event_coords(evt, board) {
-    // Note that the event coordinates evt.x and evt.y are relative to the top left corner of the page, rather than the board. Need to use evt.layerX and evt.layerY to do it properly.
-    var X = (evt.layerX - board.origin.scrCoords[1]) / board.unitX;
-    var Y = (board.origin.scrCoords[2] - evt.layerY) / board.unitY;
-    return [X, Y];
+function event_coords(evt, board) {
+  // Note that the event coordinates evt.x and evt.y are relative to the top left corner of the page, rather than the board. Need to use evt.layerX and evt.layerY to do it properly.
+  var X = (evt.layerX - board.origin.scrCoords[1]) / board.unitX;
+  var Y = (board.origin.scrCoords[2] - evt.layerY) / board.unitY;
+  return [X, Y];
 }
